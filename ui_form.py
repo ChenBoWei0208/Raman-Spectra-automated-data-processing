894edcde28cf3d686fb3ecf7f43ee0fda2951763
Chen Bo Wei <U11187021@mail2.mcut.edu.tw>
2026-07-07 20:41:26 +0800

    feat-smooth, graph stack/non stack

--- ui_form.py
+++ ui_form.py
@@ -15,23 +15,39 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QPushButton, QSizePolicy, QTextEdit,
-    QWidget)
+from PySide6.QtWidgets import (QApplication, QComboBox, QPushButton, QSizePolicy,
+    QTextEdit, QVBoxLayout, QWidget)
 
 class Ui_Widget(object):
     def setupUi(self, Widget):
         if not Widget.objectName():
             Widget.setObjectName(u"Widget")
-        Widget.resize(800, 600)
-        self.Open_File = QPushButton(Widget)
-        self.Open_File.setObjectName(u"Open_File")
-        self.Open_File.setGeometry(QRect(230, 220, 80, 24))
-        self.view = QTextEdit(Widget)
-        self.view.setObjectName(u"view")
-        self.view.setGeometry(QRect(340, 150, 291, 201))
+        Widget.resize(818, 735)
+        Widget.setMinimumSize(QSize(818, 0))
+        Widget.setMaximumSize(QSize(818, 16777215))
+        self.verticalLayoutWidget = QWidget(Widget)
+        self.verticalLayoutWidget.setObjectName(u"verticalLayoutWidget")
+        self.verticalLayoutWidget.setGeometry(QRect(70, 130, 721, 481))
+        self.layout = QVBoxLayout(self.verticalLayoutWidget)
+        self.layout.setObjectName(u"layout")
+        self.layout.setContentsMargins(0, 0, 0, 0)
         self.Save_File = QPushButton(Widget)
         self.Save_File.setObjectName(u"Save_File")
-        self.Save_File.setGeometry(QRect(230, 280, 80, 24))
+        self.Save_File.setGeometry(QRect(80, 10, 61, 24))
+        self.Open_File = QPushButton(Widget)
+        self.Open_File.setObjectName(u"Open_File")
+        self.Open_File.setGeometry(QRect(9, 9, 61, 24))
+        self.textEdit = QTextEdit(Widget)
+        self.textEdit.setObjectName(u"textEdit")
+        self.textEdit.setGeometry(QRect(90, 640, 701, 71))
+        self.stack_box = QComboBox(Widget)
+        self.stack_box.addItem("")
+        self.stack_box.addItem("")
+        self.stack_box.setObjectName(u"stack_box")
+        self.stack_box.setGeometry(QRect(150, 10, 111, 24))
+        self.filterButton = QPushButton(Widget)
+        self.filterButton.setObjectName(u"filterButton")
+        self.filterButton.setGeometry(QRect(10, 40, 131, 24))
 
         self.retranslateUi(Widget)
 
@@ -40,7 +56,11 @@ class Ui_Widget(object):
 
     def retranslateUi(self, Widget):
         Widget.setWindowTitle(QCoreApplication.translate("Widget", u"Widget", None))
-        self.Open_File.setText(QCoreApplication.translate("Widget", u"Open File", None))
         self.Save_File.setText(QCoreApplication.translate("Widget", u"Save File", None))
+        self.Open_File.setText(QCoreApplication.translate("Widget", u"Open File", None))
+        self.stack_box.setItemText(0, QCoreApplication.translate("Widget", u"non-stack chart", None))
+        self.stack_box.setItemText(1, QCoreApplication.translate("Widget", u"stack chart", None))
+
+        self.filterButton.setText(QCoreApplication.translate("Widget", u"Savitzky-Golay filter", None))
     # retranslateUi
 
--- widget.py
+++ widget.py
@@ -3,10 +3,13 @@ import sys
 # 1. 統一全部從 PySide6 匯入（不要混用 PyQt6）
 from PySide6.QtCore import Slot
 from PySide6.QtWidgets import QApplication, QFileDialog, QWidget
-
+import pyqtgraph as pg
 # Important:
 # pyside6-uic form.ui -o ui_form.py
 from ui_form import Ui_Widget
+import pyqtgraph.exporters as exporters
+from scipy.signal import savgol_filter
+import random
 
 
 class Widget(QWidget):
@@ -14,29 +17,61 @@ class Widget(QWidget):
         super().__init__(parent)
         self.ui = Ui_Widget()
         self.ui.setupUi(self)
+        self.chart = pg.PlotWidget()
+
+
+    def update_chart(self, is_filter = False):
+        if is_filter:
+            width = 3
+        else :
+            width = 1
+        current_mode = self.ui.stack_box.currentText()
+        random_color = (random.randint(50, 255), random.randint(50, 255), random.randint(50, 255))
+        if current_mode == "stack chart":
+            self.chart.plot(self.x_data, self.y_data, pen=pg.mkPen(random_color, width=width))
+        elif current_mode == "non-stack chart":
+            self.chart.clear()
+            self.chart.plot(self.x_data, self.y_data)
 
     @Slot()
     def on_Open_File_clicked(self):
         filePath, filterType = QFileDialog.getOpenFileName(filter='TXT(*.txt)')
         if filePath:
+            self.ui.layout.addWidget(self.chart)
             try:
+                self.x_data = []
+                self.y_data = []
                 with open(filePath,'r', encoding='utf-8') as f:
-                    text = f.read()
-                    self.ui.view.setPlainText(str(text))
-                    print(f"successful open:{filePath}")
+                    for line in f:
+                        line = line.strip()
+                        if not line: continue
+                        parts = line.split('\t')
+                        x = float(parts[0].strip())
+                        y = float(parts[1].strip())
+                        self.x_data.append(x)
+                        self.y_data.append(y)
+                    self.update_chart()
+                    self.ui.textEdit.setPlainText(f"successful open: {filePath}")
             except Exception as e:
-                print(f"fail open:{e}")
+               self.ui.textEdit.setPlainText(f"fail open: {e}")
+
+
+    @Slot()
+    def on_filterButton_clicked(self):
+        self.y_data = savgol_filter(self.y_data, window_length=31, polyorder=3)
+        self.update_chart(is_filter = True)
+        self.ui.textEdit.setPlainText("successful filter")
+
     @Slot()
     def on_Save_File_clicked(self):
-        filePath, filterType = QFileDialog.getSaveFileName(filter='TXT(*.txt)')
+        filePath, filterType = QFileDialog.getSaveFileName(filter='PNG(*.png)')
         if filePath:
             try:
-                with open(filePath,'w', encoding='utf-8') as f:
-                    text = self.ui.view.toPlainText()
-                    f.write(str(text))
-                    print(f"successful save:{filePath}")
+                    exporter = exporters.ImageExporter(self.chart.plotItem)
+                    exporter.export(filePath)
+                    self.ui.textEdit.setPlainText(f"successful save: {filePath}")
             except Exception as e:
-                print(f"failure save{e}")
+                self.ui.textEdit.setPlainText(f"failure save: {e}")
 
 
 
